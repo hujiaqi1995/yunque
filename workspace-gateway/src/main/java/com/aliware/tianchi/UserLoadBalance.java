b988367832e796ac4ea8403929c7dda2a4585c03
hujiaqi1995 <jqhu340@gmail.com>
2019-07-18 23:07:20 +0800

    .

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -25,7 +25,7 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class UserLoadBalance implements LoadBalance {
 
-    public static final String NAME = "random";
+    public static final String NAME = "leastactive";
 
     private final Random random = new Random();
 
@@ -33,73 +33,28 @@ public class UserLoadBalance implements LoadBalance {
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
 
         int length = invokers.size();
-        int totalWeight = 0;
-        boolean sameWeight = true;
 
-        // 下面这个循环有两个作用，第一是计算总权重 totalWeight，
-        // 第二是检测每个服务提供者的权重是否相同
+        // 1. 获取 活跃数/权重 数组，取最小值，如果有最小值，直接调用对应的，如果相等，随机调用一个
+        int minRate = 0;
+        int least = -1;
+        int[] rates = new int[length];
+
         for (int i = 0; i < length; i++) {
-            int weight = getWeight(invokers.get(i), invocation);
-            // 累加权重
-            totalWeight += weight;
-            // 检测当前服务提供者的权重与上一个服务提供者的权重是否相同，
-            // 不相同的话，则将 sameWeight 置为 false。
-            if (sameWeight && i > 0
-                    && weight != getWeight(invokers.get(i - 1), invocation)) {
-                sameWeight = false;
-            }
+            Invoker<T> invoker = invokers.get(i);
+            int active = RpcStatus.getStatus(invoker.getUrl(), invocation.getMethodName()).getActive();
+            int weight = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
+            rates[i] = active / weight;
         }
 
-        // 下面的 if 分支主要用于获取随机数，并计算随机数落在哪个区间上
-        if (totalWeight > 0 && !sameWeight) {
-            // 随机获取一个 [0, totalWeight) 区间内的数字
-            int offset = random.nextInt(totalWeight);
-            // 循环让 offset 数减去服务提供者权重值，当 offset 小于0时，返回相应的 Invoker。
-            // 举例说明一下，我们有 servers = [A, B, C]，weights = [5, 3, 2]，offset = 7。
-            // 第一次循环，offset - 5 = 2 > 0，即 offset > 5，
-            // 表明其不会落在服务器 A 对应的区间上。
-            // 第二次循环，offset - 3 = -1 < 0，即 5 < offset < 8，
-            // 表明其会落在服务器 B 对应的区间上
-            for (int i = 0; i < length; i++) {
-                // 让随机值 offset 减去权重值
-                offset -= getWeight(invokers.get(i), invocation);
-                if (offset < 0) {
-                    // 返回相应的 Invoker
-                    return invokers.get(i);
-                }
+        int flag = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < length; i++) {
+            if (rates[i] < min) {
+                min = rates[i];
+                flag = i;
             }
         }
-
-        // 如果所有服务提供者权重值相同，此时直接随机返回一个即可
-        return invokers.get(random.nextInt(length));
-    }
-
-
-    protected int getWeight(Invoker<?> invoker, Invocation invocation) {
-        // 从 url 中获取权重 weight 配置值
-        int weight = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
-        if (weight > 0) {
-            // 获取服务提供者启动时间戳
-            long timestamp = invoker.getUrl().getParameter(Constants.REMOTE_TIMESTAMP_KEY, 0L);
-            if (timestamp > 0L) {
-                // 计算服务提供者运行时长
-                int uptime = (int) (System.currentTimeMillis() - timestamp);
-                // 获取服务预热时间，默认为10分钟
-                int warmup = invoker.getUrl().getParameter(Constants.WARMUP_KEY, Constants.DEFAULT_WARMUP);
-                // 如果服务运行时间小于预热时间，则重新计算服务权重，即降权
-                if (uptime > 0 && uptime < warmup) {
-                    // 重新计算服务权重
-                    weight = calculateWarmupWeight(uptime, warmup, weight);
-                }
-            }
-        }
-        return weight;
-    }
-
-    static int calculateWarmupWeight(int uptime, int warmup, int weight) {
-        // 计算权重，下面代码逻辑上形似于 (uptime / warmup) * weight。
-        // 随着服务运行时间 uptime 增大，权重计算值 ww 会慢慢接近配置值 weight
-        int ww = (int) ((float) uptime / ((float) warmup / (float) weight));
-        return ww < 1 ? 1 : (ww > weight ? weight : ww);
+        // 如果比例相同，请求权重最高的
+        return invokers.get(flag);
     }
 }
