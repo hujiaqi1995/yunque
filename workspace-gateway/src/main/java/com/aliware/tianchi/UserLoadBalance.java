874f00beacea5734fa90f870da72c00cec912d7b
hujiaqi1995 <jqhu340@gmail.com>
2019-07-18 23:40:48 +0800

    .

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -31,30 +31,101 @@ public class UserLoadBalance implements LoadBalance {
 
     @Override
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
-
         int length = invokers.size();
+        // 最小的活跃数
+        int leastActive = -1;
+        // 具有相同“最小活跃数”的服务者提供者（以下用 Invoker 代称）数量
+        int leastCount = 0;
+        // leastIndexs 用于记录具有相同“最小活跃数”的 Invoker 在 invokers 列表中的下标信息
+        int[] leastIndexs = new int[length];
+        int totalWeight = 0;
+        // 第一个最小活跃数的 Invoker 权重值，用于与其他具有相同最小活跃数的 Invoker 的权重进行对比，
+        // 以检测是否“所有具有相同最小活跃数的 Invoker 的权重”均相等
+        int firstWeight = 0;
+        boolean sameWeight = true;
 
-        // 1. 获取 活跃数/权重 数组，取最小值，如果有最小值，直接调用对应的，如果相等，随机调用一个
-        int minRate = 0;
-        int least = -1;
-        int[] rates = new int[length];
-
+        // 遍历 invokers 列表
         for (int i = 0; i < length; i++) {
             Invoker<T> invoker = invokers.get(i);
+            // 获取 Invoker 对应的活跃数
             int active = RpcStatus.getStatus(invoker.getUrl(), invocation.getMethodName()).getActive();
+            // 获取权重 - ⭐️
             int weight = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
-            rates[i] = active / weight;
-        }
-
-        int flag = 0;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < length; i++) {
-            if (rates[i] < min) {
-                min = rates[i];
-                flag = i;
+            // 发现更小的活跃数，重新开始
+            if (leastActive == -1 || active < leastActive) {
+                // 使用当前活跃数 active 更新最小活跃数 leastActive
+                leastActive = active;
+                // 更新 leastCount 为 1
+                leastCount = 1;
+                // 记录当前下标值到 leastIndexs 中
+                leastIndexs[0] = i;
+                totalWeight = weight;
+                firstWeight = weight;
+                sameWeight = true;
+                // 当前 Invoker 的活跃数 active 与最小活跃数 leastActive 相同
+            } else if (active == leastActive) {
+                // 在 leastIndexs 中记录下当前 Invoker 在 invokers 集合中的下标
+                leastIndexs[leastCount++] = i;
+                // 累加权重
+                totalWeight += weight;
+                // 检测当前 Invoker 的权重与 firstWeight 是否相等，
+                // 不相等则将 sameWeight 置为 false
+                if (sameWeight && i > 0
+                        && weight != firstWeight) {
+                    sameWeight = false;
+                }
             }
         }
-        // 如果比例相同，请求权重最高的
-        return invokers.get(flag);
+
+        // 当只有一个 Invoker 具有最小活跃数，此时直接返回该 Invoker 即可
+        if (leastCount == 1) {
+            return invokers.get(leastIndexs[0]);
+        }
+
+        // 有多个 Invoker 具有相同的最小活跃数，但它们之间的权重不同
+        if (!sameWeight && totalWeight > 0) {
+            // 随机生成一个 [0, totalWeight) 之间的数字
+            int offsetWeight = random.nextInt(totalWeight);
+            // 循环让随机数减去具有最小活跃数的 Invoker 的权重值，
+            // 当 offset 小于等于0时，返回相应的 Invoker
+            for (int i = 0; i < leastCount; i++) {
+                int leastIndex = leastIndexs[i];
+                // 获取权重值，并让随机数减去权重值 - ⭐️
+                offsetWeight -= getWeight(invokers.get(leastIndex), invocation);
+                if (offsetWeight <= 0) {
+                    return invokers.get(leastIndex);
+                }
+            }
+        }
+        // 如果权重相同或权重为0时，随机返回一个 Invoker
+        return invokers.get(leastIndexs[random.nextInt(leastCount)]);
+    }
+
+    protected int getWeight(Invoker<?> invoker, Invocation invocation) {
+        // 从 url 中获取权重 weight 配置值
+        int weight = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
+        if (weight > 0) {
+            // 获取服务提供者启动时间戳
+            long timestamp = invoker.getUrl().getParameter(Constants.REMOTE_TIMESTAMP_KEY, 0L);
+            if (timestamp > 0L) {
+                // 计算服务提供者运行时长
+                int uptime = (int) (System.currentTimeMillis() - timestamp);
+                // 获取服务预热时间，默认为10分钟
+                int warmup = invoker.getUrl().getParameter(Constants.WARMUP_KEY, Constants.DEFAULT_WARMUP);
+                // 如果服务运行时间小于预热时间，则重新计算服务权重，即降权
+                if (uptime > 0 && uptime < warmup) {
+                    // 重新计算服务权重
+                    weight = calculateWarmupWeight(uptime, warmup, weight);
+                }
+            }
+        }
+        return weight;
+    }
+
+    static int calculateWarmupWeight(int uptime, int warmup, int weight) {
+        // 计算权重，下面代码逻辑上形似于 (uptime / warmup) * weight。
+        // 随着服务运行时间 uptime 增大，权重计算值 ww 会慢慢接近配置值 weight
+        int ww = (int) ((float) uptime / ((float) warmup / (float) weight));
+        return ww < 1 ? 1 : (ww > weight ? weight : ww);
     }
 }
